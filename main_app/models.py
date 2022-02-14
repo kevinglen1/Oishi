3c08e4b65b23c5b030fc27f3268979041e8a1758
Kevin <kevinpeterglen@gmail.com>
2022-02-14 14:21:17 -0500

    commented out code to fix and reset

--- main_app/models.py
+++ main_app/models.py
@@ -4,8 +4,8 @@ from datetime import date
 # Import the User
 from django.contrib.auth.models import User
 
-class Product(models.Model):
+# class Product(models.Model):
 
-  stores = models.ManyToManyField(Store)
-  # Add the foreign key linking to a user instance
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+#   stores = models.ManyToManyField(Store)
+#   # Add the foreign key linking to a user instance
+#   user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -11,33 +11,33 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
-class ProductCreate(CreateView):
-  model = Product
-  fields = ['_name', '_category', '_description', '_stores', '_postedby', '_avatar']
+# class ProductCreate(CreateView):
+#   model = Product
+#   fields = ['_name', '_category', '_description', '_stores', '_postedby', '_avatar']
   
-  # This inherited method is called when a
-  # valid cat form is being submitted
-  def form_valid(self, form):
-    # Assign the logged in user (self.request.user)
-    form.instance.user = self.request.user  # form.instance is the cat
-    # Let the CreateView do its job as usual
-    return super().form_valid(form)
+#   # This inherited method is called when a
+#   # valid cat form is being submitted
+#   def form_valid(self, form):
+#     # Assign the logged in user (self.request.user)
+#     form.instance.user = self.request.user  # form.instance is the cat
+#     # Let the CreateView do its job as usual
+#     return super().form_valid(form)
 
-def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+# def signup(request):
+#   error_message = ''
+#   if request.method == 'POST':
+#     # This is how to create a 'user' form object
+#     # that includes the data from the browser
+#     form = UserCreationForm(request.POST)
+#     if form.is_valid():
+#       # This will add the user to the database
+#       user = form.save()
+#       # This is how we log a user in via code
+#       login(request, user)
+#       return redirect('index')
+#     else:
+#       error_message = 'Invalid sign up - try again'
+#   # A bad POST or a GET request, so render signup.html with an empty form
+#   form = UserCreationForm()
+#   context = {'form': form, 'error_message': error_message}
+#   return render(request, 'registration/signup.html', context)

1fefc5d48ffabae9439f6b80e36c7ab9889672ca
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-02-14 12:59:11 -0500

    models and some views and templates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Store(models.Model):
+     name = models.CharField(max_length=50)
+     postcode=models.CharField(max_length=50)
+     contact_infor=models.CharField(max_length=50)
+     offer_delivery=models.BooleanField(default=True)
+
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  category = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  stores = models.ManyToManyField(Store)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'product_id': self.id})
+
+class Photo(models.Model):
+    url=models.CharField(max_length=200)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Photo for product_id: {self.product_id} @{self.url}"
+
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,22 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('products/', views.products_index, name='index'),
+  path('products/<int:product_id>/', views.products_detail, name='detail'),
+  path('products/create/', views.ProductCreate.as_view(), name='products_create'),
+  path('products/<int:pk>/update/', views.ProductUpdate.as_view(), name='products_update'),
+  path('products/<int:pk>/delete/', views.ProductDelete.as_view(), name='products_delete'),
+  path('products/<int:product_id>/assoc_store/<int:store_id>/', views.assoc_store, name='assoc_store'),
+  path('products/<int:product_id>/unassoc_store/<int:store_id>/', views.unassoc_store, name='unassoc_store'),
+#   path('products/<int:product_id>/add_photo/', views.add_photo, name='add_photo'),
+  path('stores/', views.StoreList.as_view(), name='stores_index'),
+  path('stores/<int:pk>/', views.StoreDetail.as_view(), name='stores_detail'),
+  path('stores/create/', views.StoreCreate.as_view(), name='stores_create'),
+  path('stores/<int:pk>/update/', views.StoreUpdate.as_view(), name='stores_update'),
+  path('stores/<int:pk>/delete/', views.StoreDelete.as_view(), name='stores_delete'),
+  path('accounts/', include('django.contrib.auth.urls')),
+  path('accounts/signup', views.signup, name='signup'),
 ]

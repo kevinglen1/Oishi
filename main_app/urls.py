ef3c98e53b721d650bd267a69b196989ed8dc891
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-02-15 15:11:14 -0500

    nav bar combination

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
   path('products/', views.products_index, name='index'),
   path('products/<int:product_id>/', views.products_detail, name='detail'),
   path('products/create/', views.ProductCreate.as_view(), name='products_create'),

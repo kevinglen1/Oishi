171689678d388693df17489bf0ccbe39dfcefce0
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-02-17 15:28:55 -0500

    change store create properties

--- main_app/views.py
+++ main_app/views.py
@@ -29,18 +29,17 @@ def search_products(request):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
+    
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
+     
       user = form.save()
-      # This is how we log a user in via code
+      
       login(request, user)
       return redirect('index')
     else:
       error_message = 'Invalid credentials - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
+ 
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
@@ -50,9 +49,9 @@ class ProductCreate(LoginRequiredMixin, CreateView):
   fields = ['name', 'category', 'description']
 
   def form_valid(self, form):
-    # Assign the logged in user
+   
     form.instance.user = self.request.user
-    # Let the CreateView do its job as usual
+    
     return super().form_valid(form)
 
 class ProductUpdate(LoginRequiredMixin, UpdateView):
@@ -91,16 +90,16 @@ def products_detail(request, product_id):
 
 @login_required
 def add_photo(request, product_id):
-	# photo-file was the "name" attribute on the <input type="file">
+	
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
     s3 = boto3.client('s3')
-    # need a unique "key" for S3 / needs image file extension too
+   
     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-    # just in case something goes wrong
+    
     try:
       s3.upload_fileobj(photo_file, BUCKET, key)
-      # build the full url string
+      
       url = f"{S3_BASE_URL}{BUCKET}/{key}"
     
       photo = Photo(url=url, product_id=product_id)
@@ -130,7 +129,7 @@ class StoreDetail(LoginRequiredMixin, DetailView):
 
 class StoreCreate(LoginRequiredMixin, CreateView):
   model = Store
-  fields = '__all__'
+  fields = ['name','postcode','contact_infor']
   success_url = '/stores/'
 
 class StoreUpdate(LoginRequiredMixin, UpdateView):
